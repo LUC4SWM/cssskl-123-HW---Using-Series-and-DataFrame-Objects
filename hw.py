8d9540d6147de6cd5c6a0686053b0276408c280f
LUC4SWM <lawoldemichael@gmail.com>
2024-04-17 23:22:43 -0700

    Done

--- hw.py
+++ hw.py
@@ -5,42 +5,15 @@ filename = 'flag.data'
 headers = ['name', 'landmass', 'zone', 'area', 'population', 'language', 'religion', 'bars', 'stripes', 'colours',
            'red', 'green', 'blue', 'gold', 'white', 'black', 'orange', 'mainhue', 'circles', 'crosses',
            'saltires', 'quarters', 'sunstars', 'crescent', 'triangle', 'icon', 'animate', 'text', 'topleft', 'botright']
+
+# Read in the data file and label the columns
 df_flag = pd.read_csv(filename, names=headers, low_memory=False)
-""" Meaning of each column:
-name: Name of the country concerned
-landmass: 1=N.America, 2=S.America, 3=Europe, 4=Africa, 5=Asia, 6=Oceania
-zone: Geographic quadrant, based on Greenwich and the Equator; 1=NE, 2=SE, 3=SW, 4=NW
-area: in thousands of square km
-population: in round millions
-language: 1=English, 2=Spanish, 3=French, 4=German, 5=Slavic, 6=Other Indo-European, 7=Chinese, 8=Arabic, 9=Japanese/Turkish/Finnish/Magyar, 10=Others
-religion: 0=Catholic, 1=Other Christian, 2=Muslim, 3=Buddhist, 4=Hindu, 5=Ethnic, 6=Marxist, 7=Others
-bars: Number of vertical bars in the flag
-stripes: Number of horizontal stripes in the flag
-colours: Number of different colours in the flag
-red: 0 if red absent, 1 if red present in the flag
-green: same for green
-blue: same for blue
-gold: same for gold (also yellow)
-white: same for white
-black: same for black
-orange: same for orange (also brown)
-mainhue: predominant colour in the flag (tie-breaks decided by taking the topmost hue, if that fails then the most central hue, and if that fails the leftmost hue)
-circles: Number of circles in the flag
-crosses: Number of (upright) crosses
-saltires: Number of diagonal crosses
-quarters: Number of quartered sections
-sunstars: Number of sun or star symbols
-crescent: 1 if a crescent moon symbol present, else 0
-triangle: 1 if any triangles present, 0 otherwise
-icon: 1 if an inanimate image present (e.g., a boat), otherwise 0
-animate: 1 if an animate image (e.g., an eagle, a tree, a human hand) present, 0 otherwise
-text: 1 if any letters or writing on the flag (e.g., a motto or slogan), 0 otherwise
-topleft: colour in the top-left corner (moving right to decide tie-breaks)
-botright: Colour in the bottom-left corner (moving left to decide tie-breaks)
-"""
 
 # Task 2:
+
+# Count the number of countries in North America
 count_NA = df_flag[df_flag['landmass'] == 1]['landmass'].count()
+
 print('*** Task 2 ***\nNumber in North America: ' + str(count_NA))
 
 # Task 3:
@@ -65,10 +38,54 @@ print('\nWithout using explicit loops:\n' + str(df_flag.groupby('landmass')['nam
 print('\n*** Task 4 ***')
 
 print('Population (in millions) for speakers of each language: ')
+
+# Total population of countries that speak each language
 grouped_populations_by_language = df_flag.groupby('language')['population'].sum()
 print(grouped_populations_by_language.sort_values(ascending=False))
 
-# Summing only using countries with populations under 50 million
+# Filtered population for countries with national populations less than 50 million
 filtered_df_flag = df_flag[df_flag['population'] < 50]
 grouped_filtered_populations_by_language = filtered_df_flag.groupby('language')['population'].sum()
-print('\nFiltered to only include countries with populations under 50 million:\n', grouped_filtered_populations_by_language.sort_values(ascending=False))
+print('\nFiltered to only include countries with populations under 50 million:\n',
+      grouped_filtered_populations_by_language.sort_values(ascending=False))
+
+""" 
+    > Q: Any conclusions you can make from the different charts? 
+    
+    > A: The charts show that languages like 6 (Other 
+    Indo-European) and 7 (Chinese) are associated with countries that have larger populations, potentially indicating 
+    their widespread use or dominance in populous regions. Filtering for countries with populations under 50 million 
+    reveals the population distribution across languages in smaller countries, highlighting the diverse linguistic 
+    landscape across different population sizes.
+"""
+
+
+# Task 5:
+print('\n*** Task 5 ***')
+
+# Employee data
+df_reps = pd.DataFrame({'Rep Name': ['Max', 'Jill', 'Fong', 'Juanita', 'Nya'],
+                        'Rep Language': [1, 2, 5, 5, 8]})
+
+# Merge flags dataset with employee dataset
+merged_df = pd.merge(df_flag, df_reps, left_on='language', right_on='Rep Language', how='inner')
+# Drop unneeded column
+merged_df.drop('Rep Language', axis=1, inplace=True)
+
+# Count the total number of representative-countries
+repped_country_count = merged_df['Rep Name'].notnull().sum()
+print("Total representative-countries: ", repped_country_count)
+
+# Task 6:
+print("\n*** Task 6 ***")
+
+# Create a pivot table to show total area of countries for each combination of landmass and language
+table = df_flag.pivot_table(values='area', aggfunc='sum', columns='language', index='landmass')
+print(table)
+"""
+    Q: What do the NaN values mean in that table?
+    
+    A: In the case of this table, the NaN values indicate that there are no countries within the dataset that possess 
+    that combination of landmass and language. For example, landmass
+
+"""
